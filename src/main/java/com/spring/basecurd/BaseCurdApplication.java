d348adb02739666b22f4505b8dc59db1b3506663
phuongnv <phuongnv03@ominext.com>
2023-06-02 15:31:23 +0700

    init

---
+++ src/main/java/com/spring/basecurd/BaseCurdApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basecurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseCurdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/basecurd/Service/ProductService.java
@@ -0,0 +1,20 @@
+package com.spring.basecurd.Service;
+
+import com.spring.basecurd.Model.Product;
+
+import java.util.List;
+
+
+public interface ProductService {
+
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
